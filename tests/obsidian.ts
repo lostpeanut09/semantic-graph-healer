350ad0982324cf2aef34729cb04bc6a29bbfa9cc
Gabriel <gabriel@example.com>
2026-05-10 20:31:02 +0200

    test: fix Obsidian mock for ItemView and other UI components

--- tests/obsidian.ts
+++ tests/obsidian.ts
@@ -30,3 +30,78 @@ export const Platform = {
     isWin: true,
     isLinux: false,
 };
+
+export class ItemView {}
+export class WorkspaceLeaf {}
+export class DropdownComponent {
+    addOption() {
+        return this;
+    }
+    setValue() {
+        return this;
+    }
+    onChange() {
+        return this;
+    }
+}
+export class Setting {
+    constructor(containerEl: HTMLElement) { (this as any).settingEl = containerEl.createDiv(); }
+    setName() {
+        return this;
+    }
+    setDesc() {
+        return this;
+    }
+    addDropdown() {
+        return this;
+    }
+    addToggle() {
+        return this;
+    }
+    addSlider() {
+        return this;
+    }
+    addText() {
+        return this;
+    }
+    addButton() {
+        return this;
+    }
+    setHeading() {
+        return this;
+    }
+    setDisabled() {
+        return this;
+    }
+    addSearch() {
+        return this;
+    }
+    addExtraButton() {
+        return this;
+    }
+}
+export class ButtonComponent {
+    constructor(containerEl: HTMLElement) {}
+    setButtonText() {
+        return this;
+    }
+    setCta() {
+        return this;
+    }
+    onClick() {
+        return this;
+    }
+    setDisabled() {
+        return this;
+    }
+    setWarning() {
+        return this;
+    }
+    setIcon() {
+        return this;
+    }
+    setTooltip() {
+        return this;
+    }
+}
+export const setCssProps = (el: HTMLElement, props: Record<string, string>) => {};
