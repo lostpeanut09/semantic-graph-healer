5b48c76eb616414144c35cf93c53f5aad0fbba9f
Gabriel <gabriel@example.com>
2026-05-18 22:27:18 +0200

    fix: fix MultiGraph type intersection and install @types
    - Added node types to tsconfig.json
    - Created graphology.d.ts to resolve graphology module and MultiGraph type
    - Installed graphology-types, updated three
    - Reinstalled graphology

---
+++ src/graphology.d.ts
@@ -0,0 +1,6 @@
+declare module 'graphology' {
+    import Graph from 'graphology-types';
+    export class MultiGraph extends Graph {}
+    export class DirectedGraph extends Graph {}
+    export default Graph;
+}
